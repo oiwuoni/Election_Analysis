82493bc3d2762acd5a10cbbaf24e22ac16036343
oiwuoni <95834646+oiwuoni@users.noreply.github.com>
2022-01-30 19:36:10 -0600

    Add files via upload

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -4,6 +4,7 @@
 # Add our dependencies.
 import csv
 import os
+from tkinter import N
 
 # Add a variable to load a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
@@ -101,18 +102,26 @@ with open(file_to_save, "w") as txt_file:
 
         # 6d: Print the county results to the terminal.
         county_results = (
-            f"{county_name}: {county_vote_percentage:.1f}% ({county_vote})\n")
+            f"{county}: {county_vote_percentage:.1f}% ({county_vote})\n")
         print(county_results)
         # 6e: Save the county votes to a text file.
-
+        txt_file.write(county_results)
         # 6f: Write an if statement to determine the winning county and get its vote count.
-
+        if (county_votes[county] > winning_county_votes):
+            winning_county = county
+            winning_county_votes = county_votes[county]
 
     # 7: Print the county with the largest turnout to the terminal.
-
+    print(
+        f"----------------------------------------\n"
+        f"Largest County Turnout: {winning_county}\n"
+        f"----------------------------------------\n")
 
     # 8: Save the county with the largest turnout to a text file.
-
+    txt_file.write(
+        f"----------------------------------------\n"
+        f"Largest County Turnout: {winning_county}\n"
+        f"----------------------------------------\n")
 
     # Save the final candidate vote count to the text file.
     for candidate_name in candidate_votes:
